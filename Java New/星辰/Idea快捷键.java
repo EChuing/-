5ab41e706b9d67973f1223d33a80fc3c0a579354
EChuing <1256720611@qq.com>
2019-06-27 11:42:14 +0800

    2019 6 27 星辰科技 - Java New

--- Java New/星辰/Idea快捷键.java
+++ Java New/星辰/Idea快捷键.java
@@ -2,12 +2,35 @@
 
 IDEA 2019 快捷键
 
-
-
-
-
-
-
-
-
 	Ctrl+Shift+R  全局搜索
+	Ctrl+Shift+?  多行注释
+	Alt+Shift+箭头  行移动
+	
+
+
+
+
+
+
+IDEA关于 
+
+	--	Tomcat 部署
+		Tomcat
+			-Server
+				Name: 命名
+				After launch : 默认浏览器启动
+				URL：调试时访问的首页
+				HTTP:端口号   
+				------------  其余的不用管
+			
+			-Deployment
+				Deployment
+
+
+
+	--	外部设备禁止访问 IEDA 项目     
+			Build,Execution, Deployment
+				-Deployment
+					Port :9999
+					勾选 Can accept external connections   "是否允许外部设备访问"
+					勾选 Allow unsigned requests	
